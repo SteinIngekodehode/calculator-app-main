0303fc57f954b83abac2a244723c91af08e7d33c
Stein-Inge2 <112552332+Stein-Inge2@users.noreply.github.com>
2023-07-14 19:36:12 +0200

    first

---
+++ frontendmentor-calculator/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="container">
+    <h1>Calc</h1> 
+     
+     </div>
+    </>
+  )
+}
+
+export default App
